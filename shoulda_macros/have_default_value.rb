eaabfc59b37d5ca05094811a92b72986db235ea6
Jason <mail@jasonlambert.co.uk>
2009-08-06 15:03:03 +0100

    remove extra context that is causing warnings due to subject block being set further up the contect heirachy

--- shoulda_macros/have_default_value.rb
+++ shoulda_macros/have_default_value.rb
@@ -2,11 +2,9 @@ module HaveDefaultValue
   def should_have_default_value(field, value = nil)
     klass_name = self.name.gsub(/Test$/, '').constantize
  
-    context "#{klass_name}" do
-      should "have a default value of '#{value}' for '#{field}'" do
-          klass = subject
-          assert_equal klass.send(field), value, "#{klass_name} does not have a default value of '#{value}' for '#{field}'"
-      end
+    should "have a default value of '#{value}' for '#{field}'" do
+      klass = subject
+      assert_equal klass.send(field), value, "#{klass_name} does not have a default value of '#{value}' for '#{field}'"
     end
   end
 end
